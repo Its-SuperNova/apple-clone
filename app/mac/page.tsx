80fbf6955d0abebc3c2887c73a5c59a066ae30b8
Its-SuperNova <its.ashwin.23@gmail.com>
2025-04-14 14:45:16 +0530

    mac why apple section completed

--- app/mac/page.tsx
+++ app/mac/page.tsx
@@ -5,6 +5,7 @@ import MacHero from "@/components/pages/VideoHero";
 import styles from "./styles.module.css";
 import FeatureCards from "@/components/pages/features";
 import Lineup from "@/components/pages/lineup";
+import WhyAppleBest from "@/components/pages/whyAppleBest";
 export default function MacPage() {
   return (
     <div className="pt-12">
@@ -12,6 +13,7 @@ export default function MacPage() {
       <MacHero />
       <FeatureCards />
       <Lineup />
+      <WhyAppleBest />
     </div>
   );
 }
